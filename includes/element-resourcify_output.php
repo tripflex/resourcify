18fcf6a95755d69e6d7bf5d8431b2af7ac4dc150
Myles McNamara <myles@smyl.es>
2014-03-16 00:52:34 -0400

    durrr use wp built in funcs!

--- includes/element-resourcify_output.php
+++ includes/element-resourcify_output.php
@@ -1,14 +1,6 @@
 <?php
 	global $post;
 
-	function format_url($url) {
-	    if(!(strpos($url, "http://") === 0)
-		&& !(strpos($url, "https://") === 0)) {
-	        $url = "http://$url";
-		}
-	    return $url;
-	}
-
 	$sources = get_post_meta($post->ID,'resourcify_sources',TRUE);
 
 	$total_sources = count($sources['source_title']);
@@ -23,9 +15,8 @@
 			$source_title = $sources['source_title'][$i];
 			$source_url = $sources['source_url'][$i];
 
-			if ($source_url) $source_url = format_url($source_url);
-
 			if ($source_url != ''){
+				$source_url = esc_url_raw($source_url);
 				if ($source_title === '') $source_title = $source_url;
 				$source_html .= '<li><a target="_blank" href="' . $source_url . '" title="' . $source_title . '">' . $source_title . '</a></li>';
 			}
